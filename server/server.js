d14b2b495cd756ec56138be20bde982047fff549
Fearnbuster <sethfearnside@yahoo.com>
2018-05-04 17:01:47 -0500

    Setup app for Heroku

--- server/server.js
+++ server/server.js
@@ -12,6 +12,9 @@ let { User } = require('./models/user');
 
 let app = express();
 
+const port = process.env.PORT || 3000;
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -59,8 +62,8 @@ app.get('/todos/:id', (req, res)=>{
     });
 });
 
-app.listen(3000, ()=>{
-  console.log('Started on port 3000...');
+app.listen(port, ()=>{
+  console.log(`Started on port ${port}...`);
 });
 
 
